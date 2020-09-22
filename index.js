878e4c1419d5003f53befcffccca7930a0904d55
Ustimenko Denis <ustimenkodenis@mail.ru>
2020-09-22 22:40:44 +0300

    feat: add handlebars

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express')
+const exphbrs = require('express-handlebars')
 
 const app = express();
+const hbs = exphbrs.create({
+    defaultLayout: 'main',
+    extname: 'hbs',
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
 
 const PORT = process.env.PORT || 3000
 
