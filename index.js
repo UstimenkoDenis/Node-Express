c6de04d8ddb212e4720af50db4ecc5a8b11d2310
Ustimenko Denis <ustimenkodenis@mail.ru>
2020-09-24 21:35:57 +0300

    feat: add form

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.set('views', 'views')
 
 app.use(express.static('public'))
 
+app.use(express.urlencoded({extended: true}))
+
 app.use('/', homeRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/add', addRoutes)
